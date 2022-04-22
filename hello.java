a7df62eab576c8628038fd42478b49329f4b34d1
wty <w878579578@163.com>
2022-04-22 20:32:24 +0800

    my ninth commit

--- hello.java
+++ hello.java
@@ -2,8 +2,8 @@ pubilc static void main(String[] agrs){
 
 	System.out.println("hello world");
 	System.out.println("hello java");
-	System.out.println("w");
 	System.out.println("hello");
+	System.out.println("w");
 	System.out.println("t");
 	System.out.println("y");
 }
